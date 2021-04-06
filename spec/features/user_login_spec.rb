9f20810dbc8333dfdcaa57e29a81c0e011b92e30
Kevinliao0857 <kevinliao0857@gmail.com>
2021-04-06 22:42:40 +0000

    user login

---
+++ spec/features/user_login_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+RSpec.feature "UserLogins", type: :feature, js: true do
+  before :each do
+
+    @user = User.create! email: 'link@example.com', firstname: 'Link', lastname: 'Zelda', password: 'hyrule', password_confirmation: 'hyrule'
+
+  end
+
+  scenario "user login" do
+    visit '/'
+    first('ul.navbar-right').find_link('Login').click
+    fill_in 'email', with: 'link@example.com'
+    fill_in 'password', with: 'hyrule'
+    click_on 'Submit'
+
+    expect(page).to have_content 'Signed in as Link'
+    save_screenshot
+
+  end
+
+end
